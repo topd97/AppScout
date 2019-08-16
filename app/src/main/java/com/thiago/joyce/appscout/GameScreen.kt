2a70156e3f19cb4aac587480e9634a7c8d914948
topd97 <topd97@yahoo.com.br>
2019-08-16 17:43:12 -0300

    test

--- app/src/main/java/com/thiago/joyce/appscout/GameScreen.kt
+++ app/src/main/java/com/thiago/joyce/appscout/GameScreen.kt
@@ -8,5 +8,6 @@ class GameScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_screen)
+
     }
 }
