67e6ecb4426dcad6340032a232f0aca4db2997d8
Thiago Damasceno <topd97@yahoo.com.br>
2019-08-22 19:42:05 -0300

    Adicionadno as dependencias de safeArgs (sucesso dessa vez)

--- app/src/main/java/com/thiago/joyce/appscout/GamesListFragment.kt
+++ app/src/main/java/com/thiago/joyce/appscout/GamesListFragment.kt
@@ -1,16 +1,23 @@
 package com.thiago.joyce.appscout
 
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_games_list_screen.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class GamesListFragment : androidx.fragment.app.Fragment() {
+
+    var gamesArray: Array<String> = emptyArray()
+    var ramo_selecionado: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +26,51 @@ class GamesListFragment : androidx.fragment.app.Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_games_list, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(arguments !=null){
+            val argumentsBundle = arguments as Bundle
+            val args: GamesListFragmentArgs = GamesListFragmentArgs.fromBundle(argumentsBundle)
+            ramo_selecionado = args.selectedRamo
+        }
+
+        ramoLabel.setText(ramo_selecionado)
+
+        //Configurações
+        configureList()
+//        filterButton.setOnClickListener { changePage(FilterScreen::class.java) }
+    }
+
+    fun configureList(){
+
+        var listView = lista_jogos
+
+        //Aqui vamos pegar os dados do banco
+        gamesArray+= "test1"
+        gamesArray+= "test2"
+        gamesArray+= "test3"
+        gamesArray+= "test4"
+        gamesArray+= "test5"
+        gamesArray+= "test6"
+
+        if( context != null) {
+            val contexto = context as Context
+            var arrayAdapter = GameAdapter(contexto, gamesArray)
+            listView.adapter = arrayAdapter
+        }
+
+//        listView.setOnItemClickListener { parent, view, position, id ->
+//            changePage(GameScreen::class.java)
+//        }
+    }
+
+//    fun changePage(classe: Class<*>){
+//        val intent = Intent(this, classe)
+//        startActivity(intent)
+//    }
+
 
 
 }
--- app/src/main/java/com/thiago/joyce/appscout/RamosFragment.kt
+++ app/src/main/java/com/thiago/joyce/appscout/RamosFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_ramos.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +23,26 @@ class RamosFragment : androidx.fragment.app.Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ramos, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lobinhoButton.setOnClickListener{goToGamesList("Lobinho")};
+        escoteiroButton.setOnClickListener{goToGamesList("Escoteiro")};
+        seniorButton.setOnClickListener{goToGamesList("Senior")};
+        pioneiroButton.setOnClickListener{goToGamesList("Pioneiro")};
+        anyButton.setOnClickListener{goToGamesList("Qualquer")};
+
+
+
+    }
+
+    fun goToGamesList(ramo: String){
+        val action: RamosFragmentDirections.RamoChoosedGoToGamesList = RamosFragmentDirections.ramoChoosedGoToGamesList()
+        action.setSelectedRamo(ramo)
+        findNavController().navigate(action)
+    }
+
 
 
 }
