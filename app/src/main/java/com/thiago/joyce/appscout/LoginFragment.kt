1e1d2eab9c2551f05dd1176efbc579150e48c977
Thiago Damasceno <topd97@yahoo.com.br>
2019-08-22 18:25:52 -0300

    Adicionando Ramos e games List
    Adicionando os fragments de Ramos e de GamesList
    Colocado a transição do Login pro ramos

--- app/src/main/java/com/thiago/joyce/appscout/LoginFragment.kt
+++ app/src/main/java/com/thiago/joyce/appscout/LoginFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +21,13 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_log_in.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.logedAndGoToRamosScreen))
+    }
+
 
 
 }
