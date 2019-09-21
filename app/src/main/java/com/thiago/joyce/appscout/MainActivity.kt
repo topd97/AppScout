fef342d1ab0504bfceb5c5ab6f04e6a91097a46d
Thiago Damasceno <topd97@yahoo.com.br>
2019-09-20 23:24:19 -0300

    tentativa de conectar a um banco online

--- app/src/main/java/com/thiago/joyce/appscout/MainActivity.kt
+++ app/src/main/java/com/thiago/joyce/appscout/MainActivity.kt
@@ -2,11 +2,31 @@ package com.thiago.joyce.appscout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val connectionProps = Properties()
+        connectionProps.put("user", "esuGU1HeuI")
+        connectionProps.put("password", "CEfR2jPWMw")
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance()
+            val conn = DriverManager.getConnection(
+                "jdbc:" + "mysql" + "://" +
+                        "remotemysql.com/phpmyadmin/index.php?db=esuGU1HeuI",
+                connectionProps)
+        } catch (ex: SQLException) {
+            // handle any errors
+            ex.printStackTrace()
+        } catch (ex: Exception) {
+            // handle any errors
+            ex.printStackTrace()
+        }
     }
 }
